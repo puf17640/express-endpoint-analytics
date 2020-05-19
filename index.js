c1604d059f19df6c2245d0785ae08ec160cc18a3
Julian Pufler <puf17640@spengergasse.at>
2020-05-19 13:35:43 +0200

    options

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const analytics = { }
-export default {
-  middleware: (options) => {
+module.exports = {
+  middleware: (options = { excludeEndpoints: [] , excludeActions: []}) => {
+    options.excludeActions = options.excludeActions.map((action) => action.toLowerCase())
     return function(req, _res, next) {
-      analytics[req.path] ? analytics[req.path]++ : (analytics[req.path] = 1)
+      if (!options.excludeEndpoints.includes(req.path) 
+        && !options.excludeActions.includes(req.method.toLowerCase()))
+        analytics[req.path] ? analytics[req.path]++ : (analytics[req.path] = 1)
       next()
     }
   },
